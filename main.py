f7fe116754e64c364bb1d4cf38281665c974d5be
Esley <esleynathan@hotmail.com>
2023-01-04 00:41:09 -0300

    Desenvolvimento Web - API | PROJETO 2 - AUTENTICAÇÃO + MYSQL | CADASTRO

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from fastapi import FastAPI
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import datetime
+from models import CONN,Pessoa,Tokens
+from secrets import token_hex
 
-USUARIO = 'root'
-SENHA = ''
-HOST = 'localhost'
-BANCO = 'aulafastapi'
-PORT = '3306'
+app = FastAPI()
 
-CONN = f'mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}'
+def conectaBanco():
+    engine = create_engine(CONN, echo=True)
+    Session = sessionmaker(bind= engine)
+    return Session()
 
-engine = create_engine(CONN, echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
 
-class Pessoa(Base):
-    __tablename__ = 'Pessoa'
-    id = Column(Integer, primary_key=True)
-    nome = Column(String(50))
-    usuario = Column(String(20))    
-    senha = Column(String(10))
+@app.post('/cadastro')
+def cadastro(nome: str, user: str, senha: str):
+    session = conectaBanco()
+    usuario = session.query(Pessoa).filter_by(usuario=user).all()
+    if len(usuario) == 0:
+        x = Pessoa(nome= nome, usuario=user, senha=senha)
+        session.add(x)
+        session.commit()
+        return {'status': 'sucesso'}
+    elif len(usuario) > 0:
+        return {'status': 'Usuario já cadastrado'}
 
-class Tokens(Base):
-    __tablename__ = 'Tokens'    
-    id = Column(Integer, primary_key=True)
-    id_pessoa = Column(Integer, ForeignKey('Pessoa.id'))
-    token = Column(String(100))
-    data = Column(DateTime, default=datetime.datetime.utcnow())
-
-Base.metadata.create_all(engine)
