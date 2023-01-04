c4bb0e78f1e1bf89cd68a337cbf2102776bc8788
Esley <esleynathan@hotmail.com>
2023-01-03 23:03:31 -0300

    Desenvolvimento Web - API - Pydantic

--- req.py
+++ req.py
@@ -2,4 +2,4 @@ import requests
 
 retorno = requests.get("http://127.0.0.1:8000")
 
-print(retorno.json()['mensagem'])
+print(retorno.json())
--- teste.py
+++ teste.py
@@ -3,12 +3,23 @@ from pydantic import BaseModel
 from typing import Optional
 app = FastAPI()
 
-usuarios = [(1, 'esley','minhasenha'),(2, 'camila', 'senhacamila'),(3, 'sarah', 'senhasarah')]
+class Usuario(BaseModel):
+    id: int
+    nome: str
+    senha: str
+
+lista = [
+        Usuario(id=1, nome='nome1', senha='senha1'),
+        Usuario(id=2, nome='nome2', senha='senha2'),
+        Usuario(id=3, nome='nome3', senha='senha3'),
+        Usuario(id=4, nome='nome4', senha='senha4')
+]
 
 @app.post('/usuario')
-def main(nome):
-    for i in usuarios:
-        if i[1] == nome:
-            return i
-    
-    return "Não exisgte esse usuario"
+def main(usuario: Usuario):
+    lista.append(usuario)       
+    return (f'O Usuario {usuario} foi cadsatrado com sucesso.')
+
+@app.get('/usuarioListar')
+def main():       
+    return lista
