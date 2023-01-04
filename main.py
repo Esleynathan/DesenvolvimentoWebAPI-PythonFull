62a08b414833cad9c55295d15df3377e5036e5a7
Esley <esleynathan@hotmail.com>
2023-01-04 01:09:16 -0300

    Desenvolvimento Web - API | PROJETO 2 - AUTENTICAÇÃO + MYSQL | LOGIN

--- main.py
+++ main.py
@@ -25,3 +25,23 @@ def cadastro(nome: str, user: str, senha: str):
     elif len(usuario) > 0:
         return {'status': 'Usuario já cadastrado'}
 
+@app.post('/login')
+def login(usuario: str, senha: str):
+    session = conectaBanco()
+    user = session.query(Pessoa).filter_by(usuario=usuario, senha=senha).all()
+    if len(user) == 0:
+        return {'status': 'Usuario inexistente'}
+
+    while True:
+        token = token_hex(50)
+        tokenExiste = session.query(Tokens).filter_by(token=token).all()
+        if len(tokenExiste) == 0:
+            pessoaExiste = session.query(Tokens).filter_by(id_pessoa=user[0].id).all()
+            if len(pessoaExiste) == 0:
+                novoToken = Tokens(id_pessoa=user[0].id, token=token)
+                session.add(novoToken)
+            elif len(pessoaExiste) > 0:
+                pessoaExiste[0].token = token
+            session.commit()
+            break
+    return token
